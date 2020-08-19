39938494462bf02ebf586da2368562bd6d6d9b2c
pinto_rpc <amistad.hola@gmail.com>
2020-08-19 00:21:29 -0400

    hemos relaciondo los modelos User y Auto

--- app/Auto.php
+++ app/Auto.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Auto extends Model
 {
-    //
+    protected $table = 'autos';
+    protected $fillable = ['marca', 'modelo', 'color', 'año'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
